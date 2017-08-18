af2f2ff41ebd4b81e7c199ff3edbac791b016d8f
Mihir <mhrpatel12@gmail.com>
2017-08-18 18:49:31 +0530

    Activity for questionaires.

---
+++ app/src/main/java/com/mhr/trivia/activities/QuestionaireActivity.java
@@ -0,0 +1,15 @@
+package com.mhr.trivia.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mhr.trivia.R;
+
+public class QuestionaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionaire);
+    }
+}
